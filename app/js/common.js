63d8a3d7037249977c7429adc20d37ff4565c035
Dezmon <chernolichenko.ai@gmail.com>
2018-08-09 20:26:35 +0300

    first push

---
+++ app/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+    	items: 1,
+    	loop: true,
+    	dots: false,
+    	autoplay: true,
+    	autoplayTimeout: 7000,
+    	autoplaySpeed: 3000,
+    	dragEndSpeed: 2000,
+    });
+});
