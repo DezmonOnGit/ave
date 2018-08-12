a65414b3c758602e0dabfe3b0919aacbd7bfde9e
Dezmon <chernolichenko.ai@gmail.com>
2018-08-12 18:47:51 +0300

    add new pages

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,25 @@
+function initMap() {
+	var coordinates = {lat: 51.512471, lng: -0.139817},
+	    markerImage = '../img/marker-map.png',
+	    zoom = 20,
+
+	    map = new google.maps.Map(document.getElementById('map'), {
+	        center: coordinates,
+	        zoom: zoom,
+	        disableDefaultUI: true
+	    }),
+
+	    marker = new google.maps.Marker({
+	        position: coordinates,
+	        map: map,
+	        icon: markerImage
+	    });
+
+	$.getJSON("../json/map-style.json", function(data) {
+    	map.setOptions({styles: data});
+	});
+}
+
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
     	items: 1,
